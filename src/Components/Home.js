ff2ec7574d0e114e28f61c629a53f932725101ef
habibullahsujan <habibsujan007@gmail.com>
2022-12-29 20:07:53 +0600

    fourth commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,32 +9,43 @@ import Header from "./Header";
 import Services from "./Services";
 import Testimonials from "./Testimonials";
 import "./Styles.css";
+import { RingLoader } from "react-spinners";
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   return (
-    <div>
-      <Header />
-
-     <Banner />
-
-      <About />
-      <Counts />
-      <Services />
-      <Feature />
-      <section className="testimonial-section text-black text-center">
-        <div className="lg:w-[80%] mx-auto py-10">
-          <div className="section-title">   
-            <h2>Testimonials</h2>
-            <p>What's Our Clients Says About Us</p>
-          </div>
-          <Testimonials />
+    <>
+      {loading ? (
+      <div className="h-[100vh] flex items-center justify-center w-full">  <RingLoader color="#36d7b7" /></div>
+      ) : (
+        <div>
+          <Header />
+          <Banner />
+          <About />
+          <Counts />
+          <Services />
+          <Feature />
+          <section className="testimonial-section text-black text-center">
+            <div className="lg:w-[80%] mx-auto py-10">
+              <div className="section-title">
+                <h2>Testimonials</h2>
+                <p>What's Our Clients Says About Us</p>
+              </div>
+              <Testimonials />
+            </div>
+          </section>
+          <FAQ />
+          <Footer />
         </div>
-      </section>
-      <FAQ />
-      <Footer />
-    </div>
+      )}
+    </>
   );
 };
 
